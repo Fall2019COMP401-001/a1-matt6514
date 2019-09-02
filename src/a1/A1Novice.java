9773a71d57ccfe3aac9b172700e0a71fbd76daf6
matty6514 <matty6514@matthews-mbp.wireless-psk.unc.edu>
2019-09-02 03:40:06 -0400

    A1 Novice Code Complete

--- src/a1/A1Novice.java
+++ src/a1/A1Novice.java
@@ -9,6 +9,22 @@ public class A1Novice {
 		Scanner scan = new Scanner(System.in);
 
 		// Your code follows here.
+		int num_cus = scan.nextInt();
+		
+		for (int i = 0; i < num_cus;i++) {
+			System.out.print(scan.next().substring(0,1) + ". ");
+			System.out.print(scan.next() + ": ");
+			int num_item = scan.nextInt();
+			double total_price = 0.0;
+			for (int j = 0;j < num_item;j++) {
+				int quan = scan.nextInt();
+				String name = scan.next();
+				total_price += quan * scan.nextDouble();
+			}
+			String total = Integer.toString((int)(total_price*100));
+			System.out.println(total.substring(0,total.length()-2) + "." + total.substring(total.length()-2,total.length()));
+		}
+		
 		
 	}
 }
